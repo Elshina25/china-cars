d32622d8d5ffeeff66fdfb8842f8407e3c561d21
Анастасия Ельшина <salnaya92@gmail.com>
2024-06-27 01:53:25 +0300

    project is done till request price block

--- js/script.js
+++ js/script.js
@@ -1,60 +1,46 @@
-$(function() {
+$(function () {
 
-    $(document).on('click', '.cars-button', function() {
+    $(document).on('click', '.cars-button', function () {
         let button = $(this);
         let tab = $(button).data('tab');
         $('.cars-button').removeClass('active');
         $('.cars-tabs-buttons').removeClass('active');
+        $('.cars-card').removeClass('active');
         $(button).addClass('active');
-        $('.item-tab-' + tab).addClass('active');
+        $('.item-' + tab).addClass('active');
     });
 
 
     $('.owl-cars').owlCarousel({
-        loop:true,
-        nav:false,
+        loop: true,
+        nav: false,
         dots: true,
-        margin:10,
+        margin: 10,
         mouseDrag: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
-    
-    
-    })
-    
-    
-    // $('.owl-items_set').owlCarousel({
-    //     loop:true,
-    //     nav:true,
-    //     dots: true,
-    //     margin:10,
-    //     mouseDrag: true,
-    //     responsiveClass:true,
-    //     responsive:{
-    //         0:{
-    //             items:2
-    //         },
-    //         600:{
-    //             items:2
-    //         },
-    //         1000:{
-    //             items:4
-    //         }
-    //     }
-    
-       
-    // })
 
+
+    })
 
 });
 
+// добавление класса элементу инфографики для заливки фона через один элемент
+document.addEventListener('DOMContentLoaded', () => {
+    const elemArray = Array.from(document.querySelectorAll('.about-item'));
+    elemArray.forEach((div, i) => {
+        if (i % 2 === 1) {
+            div.classList.add('orange');
+        }
+    })
+})
