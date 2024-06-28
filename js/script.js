8edb829274411c09fd2b01dc15b427a7647e0a03
Анастасия Ельшина <salnaya92@gmail.com>
2024-06-28 17:35:54 +0300

    project done till cases clients

--- js/script.js
+++ js/script.js
@@ -35,12 +35,36 @@ $(function () {
 
 });
 
-// добавление класса элементу инфографики для заливки фона через один элемент
 document.addEventListener('DOMContentLoaded', () => {
+    // добавление класса элементу инфографики для заливки фона через один элемент
     const elemArray = Array.from(document.querySelectorAll('.about-item'));
     elemArray.forEach((div, i) => {
         if (i % 2 === 1) {
             div.classList.add('orange');
         }
-    })
-})
+    });
+
+    //добавление отступа только четным видео-элементам
+    const videoArray = Array.from(document.querySelectorAll('.video-review'));
+    videoArray.forEach((div, i) => {
+        if (i % 2 === 1) {
+            div.style.marginRight = '22px';
+        }
+    });
+
+   //добавления обработчика события клика по блоку FAQ
+   const faqBlock = document.querySelector('.faq-block');
+   faqBlock.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('faq-question')) {
+        const answerId = target.getAttribute('data-answer-id');
+        const answerElem = document.querySelector(`#${answerId}`)
+
+        if (answerElem) {
+            console.log(answerElem);
+            answerElem.classList.toggle('active');
+            target.classList.toggle('active');
+        }
+    };
+   });
+});
